f234c30e9c4ba7473c3913eeaab19ec734c82bf9
VitorMain <166560389+VitorMain@users.noreply.github.com>
2024-07-15 17:25:58 -0300

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -1,11 +1,19 @@
 import streamlit as st 
 import pandas as pd 
-import time
+
+st.set_page_config(
+  layout='wide',
+  page_title="spotify songs"
+)
 
 df = pd.read_csv('01 Spotify.csv')
+
 df.set_index('Artist', inplace=True)
-st.line_chart(df[df['Stream']> 1000000000] ['Stream'])
-df
+
+artists = df['Artist'].value_counts().index
+st.selectbox('Artista', )
+st.line_chart(df[df['Stream'] > 1000000000] ['Stream'])
+
 
 
 
